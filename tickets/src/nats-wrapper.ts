293111e2700066d9c0a81f6df18a053947017220
Mateusz Pyzowski <managervcf@gmail.com>
2020-07-24 16:49:07 +0800

    Create a singleton that exports an instance of NATS client.

---
+++ tickets/src/nats-wrapper.ts
@@ -0,0 +1,33 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+  // Define but don't initialize _client
+  private _client?: Stan;
+
+  // Getter makes sure that we called this.connect() first
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    } else {
+      return this._client;
+    }
+  }
+
+  // Connection function wrapped in a Promise
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this.client.on('connect', () => {
+        console.log('(Tickets) Connected to NATS');
+        resolve();
+      });
+
+      this.client.on('error', err => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
