ab1b31c05c95292182cbe4868b1d7992b2a4dda1
Mateusz Pyzowski <managervcf@gmail.com>
2020-08-19 13:40:36 +0800

    Fix baseURL in build-client.js file.

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -9,14 +9,8 @@ export const buildClient = ({ req }) => {
     /**
      * Axios client for the server.
      */
-
-    const isDevelopment = process.env.NODE_ENV === 'development';
-    console.log(`\n\n\n\nNODE ENVIRONMENT: ${process.env.NODE_ENV}\n\n\n\n`);
-
     return axios.create({
-      baseURL: isDevelopment
-        ? process.env.BASE_URL_DEVELOPMENT
-        : process.env.BASE_URL_PRODUCTION,
+      baseURL: process.env.BASE_URL_PRODUCTION,
       headers: req.headers,
     });
   } else {
