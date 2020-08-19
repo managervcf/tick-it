45fcd0ce7df9cef34869aa414d45859b0ebb923a
Mateusz Pyzowski <managervcf@gmail.com>
2020-08-19 12:43:41 +0800

    Add a check for development environment while invoking the build-client function.

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -9,8 +9,14 @@ export const buildClient = ({ req }) => {
     /**
      * Axios client for the server.
      */
+
+    const isDevelopment = process.env.NODE_ENV === 'development';
+    console.log(`\n\n\n\nNODE ENVIRONMENT: ${process.env.NODE_ENV}\n\n\n\n`);
+
     return axios.create({
-      baseURL: 'http://www.application-production.xyz',
+      baseURL: isDevelopment
+        ? process.env.BASE_URL_DEVELOPMENT
+        : process.env.BASE_URL_PRODUCTION,
       headers: req.headers,
     });
   } else {
--- client/next.config.js
+++ client/next.config.js
@@ -5,5 +5,8 @@ module.exports = {
   },
   env: {
     STRIPE_PUBLISHABLE_KEY: 'pk_test_81H4eK5SeZQvQn6Zkw03z78b',
+    BASE_URL_DEVELOPMENT:
+      'http://ingress-nginx-controller.kube-system.svc.cluster.local',
+    BASE_URL_PRODUCTION: 'http://www.application-production.xyz',
   },
 };
