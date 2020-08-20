e6f6e2e73a50998fc7862d11cf73e4ea87f19c8a
Mateusz Pyzowski <managervcf@gmail.com>
2020-08-21 07:48:13 +0800

    Improve styling on the index page.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,37 @@
 import Link from 'next/link';
 
 const LandingPage = ({ tickets }) => {
+  console.log(tickets);
   const ticketsData = tickets.map(({ id, title, price }) => (
-    <tr key={id}>
-      <td>{title}</td>
-      <td>{price}</td>
-      <td>
-        <Link href="/tickets/[ticketId]" as={`/tickets/${id}`}>
-          <a>Details</a>
-        </Link>
-      </td>
-    </tr>
+    <div className="card m-4 pt-2">
+      <div className="row no-gutters">
+        <div className="col-md-4">
+          <div className="card-body">
+            <h2 className="card-title">{title}</h2>
+            <span className="card-text">{new Date().toDateString()}</span>
+          </div>
+        </div>
+
+        <div className="col-md-8">
+          <div className="card-body">
+            <h3 className="text-right">${price}</h3>
+            <p className="text-right">
+              <Link href="/tickets/[ticketId]" as={`/tickets/${id}`}>
+                <a href="#" className="btn btn-primary">
+                  Buy
+                </a>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   ));
 
   return (
     <div>
-      <h1>Tickets</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>{ticketsData}</tbody>
-      </table>
+      <h2 className="heading text-center">Tickets for sale</h2>
+      {ticketsData}
     </div>
   );
 };
